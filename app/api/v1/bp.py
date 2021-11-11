74e263876eba3a65dfeb6abe7cdcd4dd79aab77a
Аксентий Виктория Георгиевна <vgaksentii@domclick.ru>
2021-11-11 16:34:31 +0300

    blueprint

--- app/api/v1/bp.py
+++ app/api/v1/bp.py
@@ -5,5 +5,7 @@ from app.api.v1.handlers.movies import MovieDetailApi, MovieListApi
 bp = Blueprint(name="movies_service_v1", import_name="movies_service", url_prefix="/api/v1")
 
 
-bp.add_url_rule(rule="/movies", view_func=MovieListApi.as_view("example1"), endpoint="movies_list")
-bp.add_url_rule(rule="/movies/<movie_id>", view_func=MovieDetailApi.as_view("example2"), endpoint="movies_detail")
+bp.add_url_rule(rule="/movies", view_func=MovieListApi.as_view("movies_list"), endpoint="movies_list")
+bp.add_url_rule(
+    rule="/movies/<movie_id>", view_func=MovieDetailApi.as_view("movies_detail"), endpoint="movies_detail"
+)
